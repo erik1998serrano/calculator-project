68540436407ca66c9f06311989ce8c8133d6bc4b
erik1998serrano <erik1998serrano@outlook.com>
2022-03-28 00:40:39 +1100

    added clear and calculation functionality

--- js/script.js
+++ js/script.js
@@ -1,35 +1,53 @@
-// display numbers on screen on button press
-let userNumberOne, myOperator, userNumberTwo;
-let calculatorDisplay = document.querySelector(".display__userInput");
-
-let pressedNum = document.querySelectorAll("button");
+// * display numbers on screen on button press
+// query selectors for buttons + operators
+const calculatorDisplay = document.querySelector(".display__userInput");
+const pressedNum = document.querySelectorAll("button");
 const allOperator = document.querySelectorAll(".operator");
 
+// press number onclick eventlistener
 for (let i = 0; i < pressedNum.length; i++) {
     pressedNum[i].addEventListener("click", () => {
+        // display number
         calculatorDisplay.textContent += pressedNum[i].textContent;
-
-        if (userNumberOne) {
-            userNumberTwo = calculatorDisplay.textContent;
-        } else if (/[-+/x]/.test(calculatorDisplay.textContent)) {
-            filteredDisplayNum = calculatorDisplay.textContent.slice(0, -1);
-            myOperator = calculatorDisplay.textContent.slice(-1);
-
-            //this fuction disables the other operator when first one is selected
-            disableOperators();
-
-            userNumberOne = filteredDisplayNum;
-
-            calculatorDisplay.textContent = "";
-        }
     });
 }
 
-// clear display
-const allClear = document.querySelector(".clear");
-allClear.addEventListener("click", clearDisplay);
+// * clear display
+// query selector for clear button
+const clearDisplay = document.querySelector(".clear");
 
-function clearDisplay() {
+// clear display onclick
+clearDisplay.addEventListener("click", () => {
     calculatorDisplay.textContent = "";
-    location.reload();
-}
+});
+
+// * calculation
+const calculate = (numOne, operator, numTwo) => {
+    switch (operator) {
+        case "+": {
+            return parseFloat(numOne) + parseFloat(numTwo);
+            break;
+        }
+
+        case "-": {
+            return parseFloat(numOne) - parseFloat(numTwo);
+            break;
+        }
+
+        case "x": {
+            return parseFloat(numOne) * parseFloat(numTwo);
+            break;
+        }
+
+        case "÷": {
+            return parseFloat(numOne) / parseFloat(numTwo);
+            break;
+        }
+
+        default:
+            alert("INVALID OPERATOR");
+            break;
+    }
+};
+
+console.log(calculate(21, "x", 10));
